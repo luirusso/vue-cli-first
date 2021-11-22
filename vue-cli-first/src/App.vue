6a0e9e8e5c03a4483a85cd2823d44869c55ccde4
luirusso <luirusso_@hotmail.it>
2021-11-22 15:23:12 +0100

    project complete

---
+++ vue-cli-first/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Title />
+    <Hero />
+  </div>
+</template>
+
+<script>
+import Title from './components/Title.vue';
+import Hero from './components/Hero.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Title,
+    Hero,
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-cli-first/src/components/Hero.vue
@@ -0,0 +1,15 @@
+<template>
+  <img src="https://alumnimathematica.org/wp-content/uploads/2016/05/Alumni-Mathematica-Blog-hello-world.gif" alt="">
+</template>
+
+<script>
+export default {
+    name: 'Hero'
+}
+</script>
+
+<style>
+    img {
+        margin: 40px 0 0
+    }
+</style>
